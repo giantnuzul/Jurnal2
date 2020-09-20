e55de6cb8e16d1baf328ca72dd9cb237fc638817
giantnuzul <giantnuzul@student.telkomuniversity.ac.id>
2020-09-20 14:25:33 +0700

    giant.java

---
+++ praktikum 2/giant.java
@@ -0,0 +1,9 @@
+public class giant {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Giant");
+		System.out.println("Nim: D3IF-6706200104");
+		System.out.println("Kelas: 44-03");
+	
+	}
+}
